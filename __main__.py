14954e5021166f88516db3259eef7a010dec2f58
Jan Zmazek <jan.zmazek@gmail.com>
2019-11-26 21:51:05 +0100

    Implemented progress bar.

--- __main__.py
+++ __main__.py
@@ -12,6 +12,15 @@ def f(name):
     abs = os.path.dirname(os.path.abspath(__file__))
     return os.path.join(abs, name)
 
+def print_progress_bar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print('\r%s |%s| %s%% %s' % (prefix, bar, percent, suffix), end = printEnd)
+    # Print New Line on Complete
+    if iteration == total:
+        print()
+
 series_location = f("{0}/{1}/series{1}.dat".format(LOCATION, DATA))
 settings_location = f("{0}/{1}/settings.yaml".format(LOCATION, DATA))
 
@@ -19,6 +28,7 @@ filtered_location = f("{0}/{1}/filtered".format(LOCATION, DATA))
 distributions_location = f("{0}/{1}/distributions".format(LOCATION, DATA))
 binarized_location = f("{0}/{1}/binarized".format(LOCATION, DATA))
 networks_location = f("{0}/{1}/networks".format(LOCATION, DATA))
+analysis_location = f("{0}/{1}/analysis".format(LOCATION, DATA))
 
 positions_location = f("{0}/{1}/positions{1}.dat".format(LOCATION, DATA))
 
@@ -33,19 +43,49 @@ with open(settings_location, 'r') as stream:
 
 # ----------------------------------  MAIN  ---------------------------------- #
 
+# Initial call to print 0% progress
+l = 14
+print_progress_bar(0, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 data = Data(series, positions, settings)
+print_progress_bar(1, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 data.filter()
+print_progress_bar(2, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 # data.plot_filtered(filtered_location)
+print_progress_bar(3, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 data.compute_distributions()
+print_progress_bar(4, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 # data.plot_distributions(distributions_location)
 data.binarize_fast()
+print_progress_bar(5, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 data.binarize_slow()
+print_progress_bar(6, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 # data.plot_binarized(binarized_location)
+print_progress_bar(7, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 data.exclude_bad_cells()
+print_progress_bar(8, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
 
 analysis = Analysis(data)
+print_progress_bar(9, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 # analysis.draw_networks(networks_location)
+print_progress_bar(10, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 # analysis.print_parameters()
+print_progress_bar(11, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 analysis.compare_slow_fast(False)
+print_progress_bar(12, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
 analysis.compare_correlation_distance(False)
+print_progress_bar(13, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
+
+analysis.plot_analysis(analysis_location)
+print_progress_bar(14, l, prefix = 'Progress:', suffix = 'Complete', length = 50)
