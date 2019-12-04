Fixed frequency calculation algorithm.

src/analysis.py
@@ -44,12 +44,6 @@ class Analysis(object):
         self.__networks = Networks(settings, self.__cells, self.__filtered_slow, self.__filtered_fast)
         self.__networks.build_networks()
 
-        # Compute network and dynamics parameters
-        self.__parameters = self.__compute_parameters()
-
-# --------------------------------- GETTERS ---------------------------------- #
-
-
 # ---------------------------- ANALYSIS FUNCTIONS ---------------------------- #
     def __search_sequence(self, arr, seq):
         # Store sizes of input array and sequence
@@ -73,7 +67,7 @@ class Analysis(object):
     def draw_networks(self, location):
         self.__networks.draw_networks(self.__positions, location)
 
-    def __compute_parameters(self):
+    def compute_parameters(self):
         G_slow = self.__networks.get_G_slow()
         G_fast = self.__networks.get_G_fast()
 
@@ -102,10 +96,19 @@ class Analysis(object):
         bin_slow = self.__binarized_slow[slice][cell]
         bin_fast = self.__binarized_fast[slice][cell]
 
-        frequency_slow = self.__search_sequence(bin_slow, [11,12]).size
-        frequency_slow = frequency_slow/len(bin_slow)*self.__sampling
-        frequency_fast = self.__search_sequence(bin_fast, [0,1]).size
-        frequency_fast = frequency_fast/len(bin_fast)*self.__sampling
+        slow_peaks = self.__search_sequence(bin_slow, [11,12])
+        if slow_peaks.size < 2:
+            frequency_slow = None
+        else:
+            slow_interval = slow_peaks[-1]-slow_peaks[0]
+            frequency_slow = (slow_peaks.size-1)/slow_interval*self.__sampling
+
+        fast_peaks = self.__search_sequence(bin_fast, [0,1])
+        if fast_peaks.size < 2:
+            frequency_fast = None
+        else:
+            fast_interval = fast_peaks[-1]-fast_peaks[0]
+            frequency_fast = (fast_peaks.size-1)/fast_interval*self.__sampling
 
         return (frequency_slow, frequency_fast)
 
@@ -136,13 +139,14 @@ class Analysis(object):
 
     def plot_analysis(self, directory):
         for slice in range(self.__slices):
+            par_values = self.compute_parameters()
             pars = ("NDs", "NDf", "Cs", "Cf", "NNDs", "NNDf", "AT", "Fs", "Ff", "ISI", "ISIV")
 
             fig, ax = plt.subplots(len(pars), len(pars), figsize=(75, 75))
             for p1 in range(len(pars)):
                 for p2 in range(p1+1):
-                    x = np.array([self.__parameters[slice][c][pars[p1]] for c in range(self.__cells)])
-                    y = np.array([self.__parameters[slice][c][pars[p2]] for c in range(self.__cells)])
+                    x = np.array([par_values[slice][c][pars[p1]] for c in range(self.__cells)])
+                    y = np.array([par_values[slice][c][pars[p2]] for c in range(self.__cells)])
 
                     both_not_None = np.logical_and(x != None, y != None)
                     x = x[both_not_None].astype(float)

src/data.py
@@ -21,7 +21,7 @@ class Data(object):
         self.__positions = positions
         self.__settings = settings
 
-        assert self.__positions.shape[0] == self.__signal.shape[0]
+        self.__check_arguments()
 
         self.__points = len(self.__time)
         self.__cells = len(self.__signal)
@@ -79,6 +79,15 @@ class Data(object):
     def get_binarized_slow(self): return self.__binarized_slow
     def get_binarized_fast(self): return self.__binarized_fast
 
+# ------------------------------ ERROR METHODS ------------------------------- #
+    def __check_arguments(self):
+        if len(self.__signal.shape) != 2:
+            raise ValueError("Series must be 2D array.")
+        elif len(self.__positions.shape) != 2 or self.__positions.shape[1] != 2:
+            raise ValueError("Positions must be 2D array with 2 columns.")
+        elif self.__positions.shape[0] != self.__signal.shape[0]:
+            raise ValueError("Number of cells in positions and signal is different.")
+
 # ----------------------------- ANALYSIS METHODS ----------------------------- #
 # ---------- Filter + smooth ---------- #
     def filter(self):
@@ -111,7 +120,6 @@ class Data(object):
         if self.__filtered_slow is False or self.__filtered_fast is False:
             raise ValueError("No filtered data!")
         for i in range(self.__cells):
-            print(i)
             mean = np.mean(self.__signal[i])
 
             fig, (ax1, ax2) = plt.subplots(2)
@@ -183,8 +191,6 @@ class Data(object):
         if self.__distributions is False:
             raise ValueError("No distribution data.")
         for cell in range(self.__cells):
-            print(cell)
-
             hist = self.__distributions[cell]["hist"]
             x = self.__distributions[cell]["bins"]
             p = self.__distributions[cell]["p"]
@@ -204,6 +210,8 @@ class Data(object):
             ax1.plot(self.__time,self.__signal[cell]-mean,linewidth=0.5,color='dimgrey', zorder=0)
             ax1.plot(self.__time,self.__filtered_fast[cell],linewidth=0.5,color='red', zorder=2)
             ax1.plot(self.__time, [p_root for i in self.__time], color="green", zorder=2)
+            ax1.axvline(x=self.__settings["analysis"]["interval"][0])
+            ax1.axvline(x=self.__settings["analysis"]["interval"][1])
             if q_root is not np.inf:
                 ax1.fill_between(self.__time, -q_root, q_root, color='orange', zorder=1)
 
@@ -295,8 +303,6 @@ class Data(object):
 
             plt.savefig("{0}/{1}.pdf".format(directory, cell), dpi=200, bbox_inches='tight')
             plt.close()
-
-            print(cell)
 
     def exclude_bad_cells(self):
         if self.__good_cells is False: