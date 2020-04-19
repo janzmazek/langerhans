Fixed slow binarization bug, added mean amplitude calculation, minor changes.

src/analysis.py
@@ -70,9 +70,6 @@ class Analysis(object):
         self.__networks.draw_networks(self.__positions, location)
 
     def compute_parameters(self):
-        if self.__networks is not False:
-            G_slow = self.__networks.get_G_slow()
-            G_fast = self.__networks.get_G_fast()
 
         par1 = [dict() for c in range(self.__cells)]
         par2 = dict()
@@ -80,15 +77,20 @@ class Analysis(object):
         if self.__networks is not False:
             par2["Rs"] = self.average_correlation()[0]
             par2["Rf"] = self.average_correlation()[1]
+            par2["Ds"] = self.connection_distances()[0]
+            par2["Df"] = self.connection_distances()[1]
+        par2["MA"] = self.mean_amplitude()
         for cell in range(self.__cells):
             par1[cell]["AD"] = self.activity(cell)[0]
             par1[cell]["AT"] = self.activity(cell)[1]
+            par1[cell]["OD"] = self.activity(cell)[2]
             par1[cell]["Fs"] = self.frequency(cell)[0]
             par1[cell]["Ff"] = self.frequency(cell)[1]
             par1[cell]["ISI"] = self.interspike(cell)[0]
             par1[cell]["ISIV"] = self.interspike(cell)[1]
-            par1[cell]["TP"] = self.start(cell)["plateau"]
-            par1[cell]["TS"] = self.start(cell)["spike"]
+            par1[cell]["TP"] = self.time(cell)["plateau_start"]
+            par1[cell]["TS"] = self.time(cell)["spike_start"]
+            par1[cell]["TI"] = self.time(cell)["plateau_end"]
 
             if self.__networks is not False:
                 par1[cell]["NDs"] = self.__networks.node_degree(cell)[0]
@@ -105,15 +107,36 @@ class Analysis(object):
             raise ValueError("Network is not built.")
         return self.__networks.average_correlation()
 
+    def connection_distances(self):
+        if self.__networks is False:
+            raise ValueError("Network is not built.")
+        A_dst = self.distances_matrix()
+        A_slow, A_fast = self.__networks.adjacency_matrix()
+
+        A_dst_slow = np.multiply(A_dst, A_slow)
+        A_dst_fast = np.multiply(A_dst, A_fast)
+
+        slow_distances, fast_distances = [], []
+        for c1 in range(self.__cells):
+            for c2 in range(c1):
+                ds = A_dst_slow[c1,c2]
+                df = A_dst_fast[c1,c2]
+                if ds > 0: slow_distances.append(ds)
+                if df > 0: fast_distances.append(df)
+
+        return (np.array(slow_distances), np.array(fast_distances))
+
+
     def activity(self, cell):
         start = int(self.__activity[cell][0]*self.__sampling)
         stop = int(self.__activity[cell][1]*self.__sampling)
         bin = self.__binarized_fast[cell][start:stop]
         sum = np.sum(bin)
         length = bin.size
+        Nf = self.frequency(cell)[1]*length/self.__sampling
         if sum == 0:
-            return (length, np.nan)
-        return (length/self.__sampling, sum/length)
+            return (length, np.nan, np.nan)
+        return (length/self.__sampling, sum/length, sum/Nf)
 
     def frequency(self, cell):
         start = int(self.__activity[cell][0]*self.__sampling)
@@ -164,20 +187,38 @@ class Analysis(object):
 
         return (mean_interspike_interval, interspike_variation)
 
-    def start(self, cell):
+    def time(self, cell):
         bin_fast = self.__binarized_fast[cell]
         time = {}
         stimulation_start = self.__settings["Stimulation [frame]"][0]
+        stimulation_end = self.__settings["Stimulation [frame]"][1]
 
-        time["plateau"] = self.__activity[cell][0] - stimulation_start/self.__sampling
+        time["plateau_start"] = self.__activity[cell][0] - stimulation_start/self.__sampling
+        time["plateau_end"] = stimulation_end/self.__sampling - self.__activity[cell][1]
+        if time["plateau_end"] < 0: time["plateau_end"] = 0
 
         fast_peaks = self.__search_sequence(bin_fast[stimulation_start:], [0,1])
         if len(fast_peaks) < 3:
-            time["spike"] = np.nan
+            time["spike_start"] = np.nan
         else:
-            time["spike"] = (np.mean(fast_peaks[:3]) - stimulation_start)/self.__sampling
+            time["spike_start"] = (np.mean(fast_peaks[:3]) - stimulation_start)/self.__sampling
 
         return time
+
+    def mean_amplitude(self):
+        amplitudes = []
+        for cell in range(self.__cells):
+            heavisided_gradient = np.heaviside(np.gradient(self.__filtered_slow[cell]), 0)
+            minima = self.__search_sequence(heavisided_gradient, [0,1])
+            maxima = self.__search_sequence(heavisided_gradient, [1,0])
+
+            if maxima[0] < minima[0]: maxima = np.delete(maxima, 0)
+            if maxima[-1] < minima[-1]: minima = np.delete(minima, 0)
+
+            for i, j in zip(minima, maxima):
+                amplitudes.append(self.__filtered_slow[cell][j]-self.__filtered_slow[cell][i])
+        return np.mean(amplitudes)
+
 
     def node_degree(self, cell):
         if self.__networks is False:
@@ -213,17 +254,26 @@ class Analysis(object):
                 spikes[phase-1] += np.sum(fast_isolated_unitized)
         return (phases, spikes)
 
-    def correlation_vs_distance(self):
-        distances = []
-        correlations_slow = []
-        correlations_fast = []
+    def distances_matrix(self):
+        A_dst = np.zeros((self.__cells, self.__cells))
         for cell1 in range(self.__cells):
             for cell2 in range(cell1):
                 x1, y1 = self.__positions[cell1,0], self.__positions[cell1,1]
                 x2, y2 = self.__positions[cell2,0], self.__positions[cell2,1]
                 distance = np.sqrt((x1-x2)**2 + (y1-y2)**2)
-                distances.append(distance)
+                A_dst[cell1, cell2] = distance
+                A_dst[cell2, cell1] = distance
+        return A_dst
 
+
+    def correlation_vs_distance(self):
+        A_dst = self.distances_matrix()
+        distances = []
+        correlations_slow = []
+        correlations_fast = []
+        for cell1 in range(self.__cells):
+            for cell2 in range(cell1):
+                distances.append(A_dst[cell1, cell2])
                 corr_slow = np.corrcoef(self.__filtered_slow[cell1],
                                         self.__filtered_slow[cell2])[0,1]
                 corr_fast = np.corrcoef(self.__filtered_fast[cell1],
@@ -231,3 +281,8 @@ class Analysis(object):
                 correlations_slow.append(corr_slow)
                 correlations_fast.append(corr_fast)
         return (distances, correlations_slow, correlations_fast)
+
+# ------------------------------ EXTRA METHODS ------------------------------- #
+
+    def mean_filtered_slow(self):
+        return np.mean(self.__filtered_slow, 0) # average over 0 axis

src/data.py
@@ -32,6 +32,7 @@ class Data(object):
 # ------------------------------- INITIALIZER -------------------------------- #
     def __init__(self):
         self.__signal = False
+        self.__mean_islet = False
         self.__time = False
         self.__settings = False
 
@@ -49,7 +50,9 @@ class Data(object):
     def import_data(self, signal):
         if not len(signal.shape)==2:
             raise ValueError("Signal shape not 2x2.")
-        self.__signal = signal[:,1:].transpose()
+        self.__signal = np.around(signal[:,1:].transpose(), decimals=3)
+        self.__mean_islet = np.mean(self.__signal, 0) # average over 0 axis
+        self.__mean_islet = self.__mean_islet - np.mean(self.__mean_islet)
         if self.__settings is False:
             self.__settings = SAMPLE_SETTINGS
         sampling = self.__settings["Sampling [Hz]"]
@@ -110,6 +113,7 @@ class Data(object):
 
         fig, ax = plt.subplots()
         ax.plot(self.__time, self.__signal[i]-mean, linewidth=0.5, color='dimgrey')
+        ax.plot(self.__time, self.__mean_islet, linewidth=0.5, color='k')
         ax.set_xlim(self.__settings["Filter"]["Plot [s]"])
         ax.set_xlabel("Time [s]")
         ax.set_ylabel("Amplitude")
@@ -276,6 +280,24 @@ class Data(object):
 
 # ---------- Binarize ---------- #
 
+    def __search_sequence(self, arr, seq):
+        # Store sizes of input array and sequence
+        seq = np.array(seq)
+        Na, Nseq = arr.size, seq.size
+
+        # Range of sequence
+        r_seq = np.arange(Nseq)
+
+        # Create a 2D array of sliding indices across the entire length of input array.
+        # Match up with the input sequence & get the matching starting indices.
+        M = (arr[np.arange(Na-Nseq+1)[:,None] + r_seq] == seq).all(1)
+
+        # Get the range of those indices as final output
+        if M.any() > 0:
+            return np.where(M == True)[0]
+        else:
+            return np.array([], dtype="int") # No match found
+
     def binarize_fast(self):
         if self.__distributions is False or self.__filtered_fast is False:
             raise ValueError("No distribution or filtered data.")
@@ -291,28 +313,22 @@ class Data(object):
         self.__binarized_slow = np.zeros((self.__cells, self.__points))
         for cell in range(self.__cells):
             signal = self.__filtered_slow[cell]
-            self.__binarized_slow[cell] = np.heaviside(np.gradient(signal), 0)
-            extremes = []
-            for i in range(1, self.__points):
-                if self.__binarized_slow[cell,i]!=self.__binarized_slow[cell,i-1]:
-                    extremes.append(i)
+            heavisided_gradient = np.heaviside(np.gradient(signal), 0)
+            minima = self.__search_sequence(heavisided_gradient, [0,1])
+            maxima = self.__search_sequence(heavisided_gradient, [1,0])
+            extremes = np.sort(np.concatenate((minima, maxima)))
 
-            up = self.__binarized_slow[0,cell]
-            counter = 0
-            for e in extremes:
-                interval = e-counter
-                for phi in range(6):
-                    lower = counter+int(np.floor(interval/6*phi))
-                    higher = counter+int(np.floor(interval/6*(phi+1)))
-                    add = 1 if up else 7
-                    self.__binarized_slow[cell,lower:higher] = phi+add
-                up = (up+1)%2
-                counter = e
+            reverse_mode = False if minima[0] < maxima[0] else True
 
-            # Erase values (from 0 to first extreme) and (last extreme to end)
             self.__binarized_slow[cell, 0:extremes[0]] = 0
+            for i in range(len(extremes)-1):
+                e1, e2 = extremes[i], extremes[i+1]
+                if i%2 == int(reverse_mode):
+                    self.__binarized_slow[cell, e1:e2] = np.floor(np.linspace(1,7,e2-e1, endpoint=False))
+                else:
+                    self.__binarized_slow[cell, e1:e2] = np.floor(np.linspace(7,13,e2-e1, endpoint=False))
             self.__binarized_slow[cell, extremes[-1]:] = 0
-            self.__binarized_slow = self.__binarized_slow.astype(int)
+        self.__binarized_slow = self.__binarized_slow.astype(int)
 
     def autolimit(self):
         if self.__binarized_fast is False: