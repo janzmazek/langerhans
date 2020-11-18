cd17dfadb6bb29794a8cdc3de80fd6d715936aae
Jan <jan.zmazek@gmail.com>
2020-11-18 18:42:23 +0100

    Added missing dependency.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
       name = "langerhans",
-      version = "1.1.0",
+      version = "1.2.0",
       description = "Analyzer for the calcium signals of Islets of Langerhans.",
       author = "Jan Zmazek",
       url = "https://github.com/janzmazek/cell-networks",
       license = "MIT License",
       packages = find_packages(exclude=['*test']),
-      install_requires = ['numpy', 'scipy', 'matplotlib', 'networkx', 'pyyaml']
+      install_requires = ['numpy', 'scipy', 'matplotlib', 'networkx', 'pyyaml', 'python-louvain']
 )
